c782b9bde1dd64435b4dce04eaab59eab6f55b30
ddii596 <adisonph@gmail.com]>
2026-01-23 17:53:14 -0300

    anexando as policyes de ec2, ssm, ecr, cloudwatch, node e cni

---
+++ iam_nodes.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role_policy_attachment" "cni" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role       = aws_iam_role.eks_cluster_role.name
+  
+}
+resource "aws_iam_role_policy_attachment" "nodes" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.eks_cluster_role.name
+  
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = aws_iam_role.eks_cluster_role.name
+
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    role       = aws_iam_role.eks_cluster_role.name
+
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonCloudWatchAgentServerPolicy"
+    role       = aws_iam_role.eks_cluster_role.name
+
+}
