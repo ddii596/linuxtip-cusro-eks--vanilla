b6a3993a69641cfbc6ca402c45bba8ea73fcc753
ddii596 <adisonph@gmail.com]>
2026-01-23 17:37:56 -0300

    renomeando arquivo

---
+++ iam_cluster.tf
@@ -0,0 +1,28 @@
+data "aws_iam_policy_document" "cluster" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = format("%s-eks-cluster-role", var.project_name)
+  assume_role_policy = data.aws_iam_policy_document.cluster.json
+
+}
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_service" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
