5847b5615b3ba1948114aa7c5c3437483ff3dc8f
ddii596 <adisonph@gmail.com]>
2026-01-23 18:03:36 -0300

    add criptografia

---
+++ kms.tf
@@ -0,0 +1,9 @@
+resource "aws_kms_key" "main" {
+    description = var.project_name
+}
+
+resource "aws_kms_alias" "main" {
+
+    name          = format("alias/%s-kms", var.project_name)
+    target_key_id = aws_kms_key.main.key_id  
+}
