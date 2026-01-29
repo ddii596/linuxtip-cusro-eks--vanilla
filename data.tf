54174aeeafcd54291f452646496a8569085a0cfa
ddii596 <adisonph@gmail.com]>
2026-01-29 18:03:56 -0300

    corrigindo nome do recuroso aws_iam_role

--- data.tf
+++ data.tf
@@ -19,3 +19,8 @@ data "aws_ssm_parameter" "pods_subnets" {
     name = var.SSM_PODS_SUBNETS[count.index]
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+    name = aws_eks_cluster.main.id
+}
+
+data "aws_caller_identity" "current" {}
